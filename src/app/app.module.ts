7642036747afe04a93f67516c72d0cc5f2f1bfb6
xuyiming <xuyimingwork@foxmail.com>
2019-07-01 00:46:35 +0800

    generate needed header pages and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    CartComponent,
+    ProductComponent,
+    ShoppingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
