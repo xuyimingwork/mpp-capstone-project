90e08ce9faebd43515c5de20fa8867f2ac3ff620
xuyiming <xuyimingwork@foxmail.com>
2019-07-01 07:27:11 +0800

    show product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { DataService } from '../data.service';
+import { DataItem } from '../data-item';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  qty = 0;
+  item: DataItem;
 
-  constructor() { }
+  constructor(private location: Location, private dataService: DataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.dataService.getData().subscribe(categories => {
+        this.item = this.dataService.getItem(categories,
+          params.get('category'), params.get('subcategory'), params.get('name'));
+      });
+    });
+  }
+
+  back() {
+    this.location.back(); // <-- go back to previous location on cancel
+  }
+
+  add() {
+    console.log(this.qty);
   }
 
 }
