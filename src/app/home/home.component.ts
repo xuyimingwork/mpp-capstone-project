208647d45643503bea30a54393e34930e7eedf09
xuyiming <xuyimingwork@foxmail.com>
2019-07-01 07:27:52 +0800

    image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { DataCategory } from '../data-category';
+import { DataItem } from '../data-item';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  items: Array<DataItem>;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getData().subscribe(categories => {
+      const items = [];
+      categories.forEach(category => {
+        category.subcategories.forEach(subcategory => {
+          subcategory.items.forEach(item => {
+            items.push(item);
+          });
+        });
+      });
+      this.items = items;
+    });
   }
 
+  getItemRouterLink(item: DataItem) {
+    return `/product/${item.category}/${item.subcategory}/${item.name}`;
+  }
 }
