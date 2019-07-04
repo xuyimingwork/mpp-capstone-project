4c360793b5b2f0cc42af976174fcbda49f9fd6fe
xuyiming <xuyimingwork@foxmail.com>
2019-07-05 01:59:31 +0800

    enable hash mode for navigate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,14 +9,17 @@ import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
+    // rubric13 The home page is accessible at http://localhost:8080/#
     path: '',
     component: HomeComponent
   },
   {
+    // rubric34 The shopping page is accessible at http://localhost:8080/#/shopping
     path: 'shopping',
     component: ShoppingComponent
   },
   {
+    // rubric46 The product page is accessible at http://localhost:8080/#/product?name=productname
     path: 'product',
     component: ProductComponent
   },
@@ -25,20 +28,24 @@ const routes: Routes = [
     component: ProductComponent
   },
   {
+    // rubric56 The cart page is accessible at http://localhost:8080/#/cart
     path: 'cart',
     component: CartComponent
   },
   {
+    // rubric62 The contact page is accessible at http://localhost:8080/#/contact
     path: 'contact',
     component: ContactComponent
   },
   {
+    // rubric64 The about page is accessible at http://localhost:8080/#/about
     path: 'about',
     component: AboutComponent
   },
 ];
 
 @NgModule({
+  // rubric13, rubric34, rubric46, rubric56, rubric62, rubric64 enable hash mode for navigate.
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
